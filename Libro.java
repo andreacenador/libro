4240ebfae22c4eebc27d5ac3cbc096aa6f4176fa
Andrea Cenador <andreacenadorgaspar@gmail.com>
2021-11-09 09:35:35 +0100

    02. Añadidos metodos imprimeAutoy y imprimeTitulo

--- Libro.java
+++ Libro.java
@@ -29,4 +29,12 @@ public class Libro {
     public String getTitulo(){
         return titulo;
     }
+    
+    public void imprimeAutor(){
+        System.out.println("El autor es " + autor);
+    }
+    
+    public void imprimeTitulo(){
+        System.out.println("El titulo es " + titulo);
+    }
 }
