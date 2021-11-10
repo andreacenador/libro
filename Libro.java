e8a636440aca08561b1a7323ab05ca0af812d365
Andrea Cenador <andreacenadorgaspar@gmail.com>
2021-11-10 10:45:01 +0100

    1847.05 Modificaciones para imprimirDetalles, getDetalles y numeroReferencia

--- Libro.java
+++ Libro.java
@@ -13,7 +13,7 @@ public class Libro {
     private String autor;
     private String titulo;
     private int numeroPaginas;
-
+    private String numeroReferencia;
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
@@ -22,37 +22,65 @@ public class Libro {
         autor = autorLibro;
         titulo = tituloLibro;
         numeroPaginas= numeroDePaginas;
+        numeroReferencia = "";
     }
-    
+
     public String getAutor(){
         return autor;
     }
-    
+
     public String getTitulo(){
         return titulo;
     }
-    
+
     public void imprimeAutor(){
         System.out.println("El autor es " + autor);
     }
-    
+
     public void imprimeTitulo(){
         System.out.println("El titulo es " + titulo);
     }
-    
+
     public int getNumeroPaginas(){
         return numeroPaginas;
     }
-    
+
     public void imprimirDetalles(){
         System.out.println("Titulo: " + titulo);
         System.out.println("Autor: " + autor);
         System.out.println("Numero de paginas: " + numeroPaginas);
+        if (numeroReferencia != ""){
+            System.out.println("Numero de refencia: " + numeroReferencia);
+        }
+        else {
+            System.out.println("Numero de referencia: zzz");
+        }
     }
-    
+
     public String getDetalles(){
-      String detalles;
-      detalles = "Titulo: " + titulo + " // Autor: " + autor + " // Numero de paginas: " + numeroPaginas;
-      return detalles;
+        String detalles;
+        if (numeroReferencia != ""){
+            numeroReferencia = numeroReferencia;    
+        }
+        else {
+            numeroReferencia = "zzz";
+        }
+
+        detalles = "Titulo: " + titulo + " // Autor: " + autor + " // Numero de paginas: " + numeroPaginas + " // Numero de Referencia: " + numeroReferencia;
+        return detalles;
+    }
+
+    public String getNumeroReferencia(){
+        return numeroReferencia;
+    }
+
+    public void setNumeroReferencia(String newNumeroReferencia){
+        if (newNumeroReferencia.length() >= 3){
+            numeroReferencia = newNumeroReferencia;
+        }
+        else {
+            System.out.println("Error, tiene que ser mas de 3 caracteres");
+        }
+        
     }
 }
